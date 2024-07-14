c28998c13e9750c84f336b20ecb1ff13518a266b
odorajbotoj <odoraj_botoj@tom.com>
2024-07-14 23:05:07 +0800

    format

--- internal/golua/golua.c
+++ internal/golua/golua.c
@@ -1,17 +1,15 @@
 #include "golua.h"
 
-const char* Lua_tostring(lua_State* L, int idx) {
-    return lua_tostring(L, idx);
+const char *Lua_tostring(lua_State *L, int idx) { return lua_tostring(L, idx); }
+
+lua_Integer Lua_tointeger(lua_State *L, int idx) {
+  return lua_tointeger(L, idx);
 }
 
-lua_Integer Lua_tointeger(lua_State* L, int idx) {
-    return lua_tointeger(L, idx);
+int LuaL_dofile(lua_State *L, const char *filename) {
+  return luaL_dofile(L, filename);
 }
 
-int LuaL_dofile(lua_State* L, const char* filename) {
-    return luaL_dofile(L, filename);
-}
-
-int Lua_pcall(lua_State* L, int n, int r, int f) {
-    return lua_pcall(L, n, r, f);
+int Lua_pcall(lua_State *L, int n, int r, int f) {
+  return lua_pcall(L, n, r, f);
 }
--- internal/golua/golua.h
+++ internal/golua/golua.h
@@ -3,16 +3,16 @@
 
 #include <stdlib.h>
 
+#include "lauxlib.h"
 #include "lua.h"
 #include "lualib.h"
-#include "lauxlib.h"
 
-const char* Lua_tostring(lua_State* L, int idx);
+const char *Lua_tostring(lua_State *L, int idx);
 
-lua_Integer Lua_tointeger(lua_State* L, int idx);
+lua_Integer Lua_tointeger(lua_State *L, int idx);
 
-int LuaL_dofile(lua_State* L, const char* filename);
+int LuaL_dofile(lua_State *L, const char *filename);
 
-int Lua_pcall(lua_State* L, int n, int r, int f);
+int Lua_pcall(lua_State *L, int n, int r, int f);
 
 #endif
